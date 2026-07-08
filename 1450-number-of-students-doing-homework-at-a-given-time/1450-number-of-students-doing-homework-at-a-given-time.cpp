11a8435a14e65c0f621e470aaaa7fbc3344a2a6d
agnavya21 <agnavya21@gmail.com>
2026-07-08 13:10:30 +0530

    Time: 0 ms (100%), Space: 14.1 MB (88.08%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n=startTime.size(), cnt=0;
+        for(int i=0;i<n;i++) {
+            if(queryTime>=startTime[i] && queryTime<=endTime[i])
+                cnt+=1;
+        }
+        return cnt;
+    }
+};
