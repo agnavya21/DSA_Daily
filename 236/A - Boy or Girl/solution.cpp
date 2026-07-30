38026d4e3fbb539f74092c678b1d8c09604ae351
Navya Agarwal <agnavya21@gmail.com>
2026-07-31 01:57:07 +0530

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    set<char> st;
+    for(char ch:s) st.insert(ch);
+    if(st.size()%2==0) cout << "CHAT WITH HER!";
+    else cout << "IGNORE HIM!";
+    return 0;
+}
