642c241800c69944ac84acb4e16fdc716f739b0d
Navya Agarwal <agnavya21@gmail.com>
2026-08-15 14:14:52 +0530

    Add HQ9+ [A] from Codeforces

---
+++ 133/A - HQ9+/solution.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    if(s.find('H')!=string::npos || s.find('Q')!=string::npos || s.find('9')!=string::npos) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
