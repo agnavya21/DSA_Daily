1bf801de0a473167f3d076e1099158d574964c78
Navya Agarwal <agnavya21@gmail.com>
2026-09-07 11:33:09 +0530

    Add Creating Words [A] from Codeforces

---
+++ 1985/A - Creating Words/solution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        string a, b;
+        cin >> a >> b;
+        string temp=a;
+        a[0]=b[0];
+        b[0]=temp[0];
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
